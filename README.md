5e0280c3e83dfce3aafe867d36f4c17caf73b7ab
devHikary <wrkkarina@gmail.com>
2021-08-01 18:52:15 -0300

    Aula 4.4

--- README.md
+++ README.md
@@ -17,6 +17,17 @@ Instrutora:  **Juliana Negreiros**
 - Configuramos o express e isolamos essa configuração;
 - Criamos uma rota POST para criar um novo atendimento e consumir essa rota usando o Postman;
 - Usamos `body-parser` para conseguir ler os dados que estamos recebendo no `body`.
+### Aula 3
+
+- Conectamos nosso servidor com o mysql;
+- Criamos tabelas no mysql usando um script node;
+- Salvamos o nosso atendimento no banco de dados.
+
+### Aula 4
+
+- Tratamos datas com MomentJS;
+- Estudamos tipos de status HTTP e quando são usados;
+- Validamos entradas antes de enviar os dados para o banco de dados.
 
 ## Anotações
 
@@ -25,3 +36,5 @@ Instrutora:  **Juliana Negreiros**
 - express
 - nodemon (utilizada somente no ambiente de desenvolvimento)
 - consign
+- mysql2
+- moment
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -3,19 +3,44 @@ const conexao = require('../infra/conexao')
 
 class Atendimento{
   adiciona(atendimento, res){
-    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-    const atendimentoDatado = {...atendimento, dataCriacao, data}
+    const dataCriacao = moment().format('YYYY-MM-DD HH:mm:ss')
+    const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss')
 
-    const sql = 'INSERT INTO Atendimentos SET ?'
+    const dataEhValida = moment(data).isSameOrAfter(dataCriacao)
+    const clienteEhValido = atendimento.cliente.length >= 5
 
-    conexao.query(sql, atendimentoDatado, (erro, resultados) =>{
-      if(erro){
-        res.status(400).json(erro)
-      } else{
-        res.status(201).json(resultados)
+    const validacoes = [
+      {
+          nome: 'data',
+          valido: dataEhValida,
+          mensagem: 'Data deve ser maior ou igual a data atual'
+      },
+      {
+          nome: 'cliente',
+          valido: clienteEhValido,
+          mensagem: 'Cliente deve ter pelo menos cinco caracteres'
       }
-    })
+    ]
+
+    const erros = validacoes.filter(campo => !campo.valido)
+    const existemErros = erros.length 
+ 
+    if(existemErros) {
+        res.status(400).json(erros)
+    } else {
+      const atendimentoDatado = {...atendimento, dataCriacao, data}
+  
+      const sql = 'INSERT INTO Atendimentos SET ?'
+  
+      conexao.query(sql, atendimentoDatado, (erro, resultados) =>{
+        if(erro){
+          res.status(400).json(erro)
+        } else{
+          res.status(201).json(atendimento)
+        }
+      })
+    }
+
   }
 }
 
